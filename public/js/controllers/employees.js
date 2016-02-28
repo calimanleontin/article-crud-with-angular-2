5c32fbef9c3f337c9279dc31ed17f08415c57f4a
calimanleontin <calimanleontin@gmail.com>
2016-02-28 22:18:40 +0200

    a little js

---
+++ public/js/controllers/employees.js
@@ -0,0 +1,8 @@
+app.controller('employeesController', function($scope, $http, API_URL){
+    $http.get(API_URL + 'employees')
+        .success(function(data){
+            $scope.employees = data;
+        });
+
+    
+});
